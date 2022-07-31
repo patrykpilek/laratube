3b2f1dcface01373b2b6d8379f7cdb9f607b57dd
Patryk Pilek <patryk.pilek@gmail.com>
2022-07-31 16:40:41 +0100

    channel upload method

--- resources/js/Pages/Channels/Upload.vue
+++ resources/js/Pages/Channels/Upload.vue
@@ -1,10 +1,36 @@
 <script setup>
+import {ref} from 'vue';
 import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue';
-import { Head } from '@inertiajs/inertia-vue3';
+import {Head, useForm} from '@inertiajs/inertia-vue3';
 
 const props = defineProps({
-    channel: Object
+    channel: Object,
+    selected: {
+        type: Boolean,
+        default: false,
+    }
 });
+
+const selected = ref(props.selected);
+
+const videoInput = ref(null);
+
+const form = useForm({
+   videos: null,
+});
+
+const selectVideo = () => {
+    videoInput.value.click();
+};
+
+const upload = () => {
+    if (videoInput.value) {
+        form.videos = videoInput.value.files[0];
+        selected.value = true
+    }
+
+    console.log(form.videos)
+};
 
 </script>
 
@@ -25,10 +51,20 @@ const props = defineProps({
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
-                        <div class="flex justify-center">
-                            <svg class="h-16 w-16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 461 461"><path d="M365.26 67.4H95.74A95.74 95.74 0 0 0 0 163.13v134.73a95.74 95.74 0 0 0 95.74 95.74h269.52A95.74 95.74 0 0 0 461 297.87V163.14a95.74 95.74 0 0 0-95.74-95.75zM300.5 237.05l-126.06 60.12a5.06 5.06 0 0 1-7.24-4.57v-124a5.06 5.06 0 0 1 7.34-4.52l126.06 63.88a5.06 5.06 0 0 1-.1 9.09z" fill="#f61c0d"/></svg>
+                        <div v-show="!selected">
+                            <div @click.prevent="selectVideo" class="flex justify-center">
+                                <svg class="h-16 w-16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 461 461"><path d="M365.26 67.4H95.74A95.74 95.74 0 0 0 0 163.13v134.73a95.74 95.74 0 0 0 95.74 95.74h269.52A95.74 95.74 0 0 0 461 297.87V163.14a95.74 95.74 0 0 0-95.74-95.75zM300.5 237.05l-126.06 60.12a5.06 5.06 0 0 1-7.24-4.57v-124a5.06 5.06 0 0 1 7.34-4.52l126.06 63.88a5.06 5.06 0 0 1-.1 9.09z" fill="#f61c0d"/></svg>
+                            </div>
+                            <p class="flex justify-center">Upload Videos</p>
+
+                            <input
+                                ref="videoInput"
+                                type="file"
+                                class="hidden"
+                                @change="upload"
+                            >
                         </div>
-                        <p class="flex justify-center">Upload Videos</p>
+
                     </div>
                 </div>
             </div>
