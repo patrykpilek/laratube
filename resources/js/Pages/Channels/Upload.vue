63fad9bed598c525cc42c5339b2cde63fac7cc31
Patryk Pilek <patryk.pilek@gmail.com>
2022-08-09 03:43:59 +0100

    update upload video to channel

--- resources/js/Pages/Channels/Upload.vue
+++ resources/js/Pages/Channels/Upload.vue
@@ -29,7 +29,9 @@ const submit = () => {
     selected.value = true
 
     form.post(route('channel.video.store', props.channel), {
+        errorBag: 'uploadVideo',
         preserveScroll: true,
+        onSuccess: () => clearVideoFileInput(),
     });
 
     console.log(form.video)
@@ -42,11 +44,17 @@ const uploadVideo = (event) => {
 
     const reader = new FileReader();
 
-    reader.onload = (e) => {
-        videoInput.video = e.target.result;
+    reader.onload = (event) => {
+        videoInput.video = event.target.result;
     };
 
     reader.readAsDataURL(video);
+};
+
+const clearVideoFileInput = () => {
+    if (videoInput.value?.value) {
+        videoInput.value.value = null;
+    }
 };
 
 </script>
@@ -67,30 +75,45 @@ const uploadVideo = (event) => {
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 bg-white border-b border-gray-200">
+
+                    <div v-if="!selected" class="px-4 py-5 sm:px-6">
                         <form @submit.prevent="submit">
-                            <div v-show="!selected">
-                                <div class="flex justify-center">
-                                    <svg class="h-16 w-16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 461 461"><path d="M365.26 67.4H95.74A95.74 95.74 0 0 0 0 163.13v134.73a95.74 95.74 0 0 0 95.74 95.74h269.52A95.74 95.74 0 0 0 461 297.87V163.14a95.74 95.74 0 0 0-95.74-95.75zM300.5 237.05l-126.06 60.12a5.06 5.06 0 0 1-7.24-4.57v-124a5.06 5.06 0 0 1 7.34-4.52l126.06 63.88a5.06 5.06 0 0 1-.1 9.09z" fill="#f61c0d"/></svg>
-                                </div>
-                                <p class="flex justify-center">Upload Videos</p>
-
-                                <input
-                                    ref="videoInput"
-                                    type="file"
-                                    accept="video/mp4"
-                                    @change="uploadVideo"
-                                >
+                            <div class="flex justify-center">
+                                <svg class="h-16 w-16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 461 461"><path d="M365.26 67.4H95.74A95.74 95.74 0 0 0 0 163.13v134.73a95.74 95.74 0 0 0 95.74 95.74h269.52A95.74 95.74 0 0 0 461 297.87V163.14a95.74 95.74 0 0 0-95.74-95.75zM300.5 237.05l-126.06 60.12a5.06 5.06 0 0 1-7.24-4.57v-124a5.06 5.06 0 0 1 7.34-4.52l126.06 63.88a5.06 5.06 0 0 1-.1 9.09z" fill="#f61c0d"/></svg>
                             </div>
+                            <p class="flex justify-center">Upload Videos</p>
 
-                            <BreezeButton class="mt-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                            <input
+                                ref="videoInput"
+                                type="file"
+                                accept="video/mp4"
+                                @change="uploadVideo"
+                            >
+                            <BreezeButton class="mt-4 flex justify-center" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
                                 Submit
                             </BreezeButton>
 
                         </form>
+                    </div>
+                    <div class="px-4 py-5 sm:p-6">
+
+                        <div class="grid grid-rows-3 grid-flow-col gap-4">
+                            <div class="row-span-1 col-span-4">
+                                <progress v-if="form.progress" :value="form.progress.percentage" max="100">
+                                    {{ form.progress.percentage }}%
+                                </progress>
+                            </div>
+                            <div class="row-span-2 col-span-2 h-48 bg-gray-100">
+                                <div class="bg-cyan-500 flex justify-center">
+                                    Loading thumbnail ...
+                                </div>
+                            </div>
+                            <div class="row-span-2 col-span-2">My awesome video</div>
+                        </div>
 
 
                     </div>
+
                 </div>
             </div>
         </div>
